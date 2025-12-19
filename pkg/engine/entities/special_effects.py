5fb7da26e6e3b0a63e66bf9d649a3c0175e8f5e4
blowa <light06120602@gmail.com>
2025-12-19 18:19:08 +0900

    special_effects.py の更新

--- pkg/engine/entities/special_effects.py
+++ pkg/engine/entities/special_effects.py
@@ -1,15 +1,24 @@
 import numpy as np
+from typing import List, Tuple, Dict, Any
+from pkg.schema.models import IActor
 
 class EffectCalculator:
     @staticmethod
-    def calculate_fear_debuff(human_pos, onis, config):
+    def calculate_fear_debuff(human_pos: Tuple[int, int], onis: List[IActor], config: Dict[str, Any]) -> float:
         fear_radius = config["human_adaptation"]["fear_radius"]
-        nearby_onis = [o for o in onis if np.linalg.norm(np.array(human_pos) - np.array(o.pos), ord=1) <= fear_radius]
+        h_p = np.array(human_pos)
         
-        if not nearby_onis:
+        nearby_count = 0
+        for o in onis:
+            if o.pos is None:
+                continue
+            if np.linalg.norm(h_p - np.array(o.pos), ord=1) <= fear_radius:
+                nearby_count += 1
+
+        if nearby_count == 0:
             return 1.0
-        return max(0.5, 1.0 - (len(nearby_onis) * 0.2))
+        return max(0.5, 1.0 - (nearby_count * 0.2))
 
     @staticmethod
-    def is_intercepted(h_prev, h_next, o_prev, o_next):
+    def is_intercepted(h_prev: Tuple[int, int], h_next: Tuple[int, int], o_prev: Tuple[int, int], o_next: Tuple[int, int]) -> bool:
         return h_prev == o_next and h_next == o_prev
