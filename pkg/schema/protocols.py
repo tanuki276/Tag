4e65a3baf22e045193c7192db66ccd9709e8cf27
blowa <light06120602@gmail.com>
2025-12-19 12:15:57 +0900

    protocols.py の更新

--- pkg/schema/protocols.py
+++ pkg/schema/protocols.py
@@ -6,20 +6,31 @@ class IActor(Protocol):
     pos: Tuple[int, int]
     is_oni: bool
     alive: bool
-    
+    escaped: bool
+
     def decide(self, view: LocalView) -> Intent:
         ...
 
     def get_public_status(self) -> Dict[str, Any]:
         ...
 
+    def commit_status(self, target_pos: Tuple[int, int], status_update: Dict[str, Any]) -> None:
+        ...
+
+    def tick(self) -> None:
+        ...
+
 @runtime_checkable
 class IState(Protocol):
     grid: Any
     actor_data: Dict[str, IActor]
+    map_elements: Dict[Tuple[int, int], Any]
     turn: int
     is_terminal: bool
-    
+    termination_reason: str
+    exit_open: bool
+    exit_pos: Tuple[int, int]
+
     def apply(self, resolved_actions: Dict[str, Action]) -> 'IState':
         ...
 
