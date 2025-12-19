59f19758c6451eeba19914ce12aa0e52f0e6db04
blowa <light06120602@gmail.com>
2025-12-19 12:07:40 +0900

    manager.py の更新

--- pkg/engine/map/manager.py
+++ pkg/engine/map/manager.py
@@ -7,19 +7,27 @@ class MapManager:
         self.grid = np.zeros((self.size, self.size), dtype=int)
         self.elements = {}
 
-    def set_wall(self, x, y):
-        if 0 <= x < self.size and 0 <= y < self.size:
-            self.grid[x, y] = 1
+    def set_wall(self, y, x):
+        if 0 <= y < self.size and 0 <= x < self.size:
+            self.grid[y, x] = 1
 
     def add_element(self, pos, element_type, data=None):
-        self.elements[pos] = MapElement(element_type, data)
+        pos = tuple(map(int, pos))
+        if 0 <= pos[0] < self.size and 0 <= pos[1] < self.size:
+            self.elements[pos] = MapElement(element_type, data)
 
     def is_wall(self, pos):
-        return self.grid[pos[0], pos[1]] == 1
+        y, x = map(int, pos)
+        if not (0 <= y < self.size and 0 <= x < self.size):
+            return True
+        return self.grid[y, x] == 1
 
     def get_element(self, pos):
-        return self.elements.get(pos)
+        return self.elements.get(tuple(map(int, pos)))
 
     def remove_element(self, pos):
+        pos = tuple(map(int, pos))
         if pos in self.elements:
             del self.elements[pos]
+            return True
+        return False
