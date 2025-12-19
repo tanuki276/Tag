286482db03cf6269ecd8c8fc928cc0e34dd72f49
blowa <light06120602@gmail.com>
2025-12-19 12:11:10 +0900

    memory.py の更新

--- pkg/entities/traits/memory.py
+++ pkg/entities/traits/memory.py
@@ -7,32 +7,31 @@ class EntityMemory:
         self.prediction_map = {}
         self.grid_map = None
 
-    def update_prediction_model(self, data):
-        for a_id, pos in data.items():
-            if a_id not in self.prediction_map:
-                self.prediction_map[a_id] = []
-            self.prediction_map[a_id].append(pos)
-            if len(self.prediction_map[a_id]) > 10:
-                self.prediction_map[a_id].pop(0)
+    def update_prediction(self, a_id, pos):
+        if a_id not in self.prediction_map:
+            self.prediction_map[a_id] = []
+        self.prediction_map[a_id].append(tuple(pos))
+        if len(self.prediction_map[a_id]) > 10:
+            self.prediction_map[a_id].pop(0)
 
     def get_relevant(self, turn):
         return {
-            "elements": self.known_elements,
-            "actors": self.seen_actors,
-            "prediction_map": self.prediction_map,
+            "elements": self.known_elements.copy(),
+            "actors": self.seen_actors.copy(),
+            "prediction_map": self.prediction_map.copy(),
             "grid_map": self.grid_map
         }
 
     def record_element(self, pos, element):
-        self.known_elements[pos] = {
+        self.known_elements[tuple(pos)] = {
             "type": element.type.name,
-            "last_seen": pos,
-            "properties": element.properties
+            "last_seen": tuple(pos),
+            "properties": element.properties.copy()
         }
 
     def update_seen_actor(self, a_id, status, turn):
         self.seen_actors[a_id] = {
-            "pos": status.get("pos"),
+            "pos": tuple(status.get("pos")) if status.get("pos") else None,
             "is_oni": status.get("is_oni"),
             "last_turn": turn
         }
