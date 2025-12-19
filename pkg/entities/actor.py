dd761daaedeac5cf0e0ad522a4d4b7a808a16e0e
blowa <light06120602@gmail.com>
2025-12-19 12:10:29 +0900

    actor.py の更新

--- pkg/entities/actor.py
+++ pkg/entities/actor.py
@@ -1,11 +1,11 @@
 import numpy as np
-from pkg.schema.protocols import IActor
 from pkg.entities.traits.memory import EntityMemory
 
 class BaseActor:
     def __init__(self, a_id, pos, config):
         self.a_id = a_id
-        self.pos = pos
+        self.pos = tuple(pos)
+        self.prev_pos = tuple(pos)
         self.config = config
         self.alive = True
         self.escaped = False
@@ -18,8 +18,21 @@ class BaseActor:
             "a_id": self.a_id,
             "pos": self.pos,
             "is_oni": self.is_oni,
-            "alive": self.alive
+            "alive": self.alive,
+            "escaped": self.escaped
         }
+
+    def commit_status(self, target_pos, status_update):
+        if target_pos is not None:
+            self.prev_pos = self.pos
+            self.pos = tuple(target_pos)
+        
+        for attr, value in status_update.items():
+            if hasattr(self, attr):
+                setattr(self, attr, value)
+
+    def tick(self):
+        pass
 
 class Human(BaseActor):
     def __init__(self, a_id, pos, config):
@@ -27,15 +40,30 @@ class Human(BaseActor):
         self.is_human = True
         self.vision_range = config["entities"]["human"]["vision_range"]
         self.has_doll = config["entities"]["human"]["initial_dolls"] > 0
+        self.stamina = 100
+        self.dream_mode = 0
+        self.invincible = False
         self.stun = 0
-        self.identifying = None
 
     def decide(self, view):
-        if self.stun > 0:
-            return type('Intent', (), {"target_pos": self.pos, "priority": 0})
+        if self.stun > 0 or not self.alive or self.escaped:
+            return type('Intent', (), {
+                "target_pos": self.pos, 
+                "priority": 0, 
+                "metadata": {}
+            })
         
-        # Logic to be implemented in sub-classes or specialized traits
-        return type('Intent', (), {"target_pos": self.pos, "priority": self.config["entities"]["human"]["move_priority"]})
+        return type('Intent', (), {
+            "target_pos": self.pos, 
+            "priority": self.config["entities"]["human"]["move_priority"],
+            "metadata": {"ignore_walls": False}
+        })
+
+    def tick(self):
+        if self.stun > 0:
+            self.stun -= 1
+        if self.dream_mode > 0:
+            self.dream_mode -= 1
 
 class Oni(BaseActor):
     def __init__(self, a_id, pos, config):
@@ -43,11 +71,21 @@ class Oni(BaseActor):
         self.is_oni = True
         self.vision_range = config["entities"]["oni"]["vision_range"]
         self.confused = 0
-        self.target_last_pos = None
 
     def decide(self, view):
+        if self.confused > 0 or not self.alive:
+            return type('Intent', (), {
+                "target_pos": self.pos, 
+                "priority": 0, 
+                "metadata": {}
+            })
+
+        return type('Intent', (), {
+            "target_pos": self.pos, 
+            "priority": self.config["entities"]["oni"]["move_priority"],
+            "metadata": {"ignore_walls": False}
+        })
+
+    def tick(self):
         if self.confused > 0:
-            return type('Intent', (), {"target_pos": self.pos, "priority": 0})
-            
-        # Logic to be implemented in sub-classes
-        return type('Intent', (), {"target_pos": self.pos, "priority": self.config["entities"]["oni"]["move_priority"]})
+            self.confused -= 1
