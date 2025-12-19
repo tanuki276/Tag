c4046800b3115f1fd51183fcf635af1115d847ec
blowa <light06120602@gmail.com>
2025-12-19 12:08:39 +0900

    mapelement.py の更新

--- pkg/engine/map/mapelement.py
+++ pkg/engine/map/mapelement.py
@@ -4,12 +4,14 @@ class ElementType(Enum):
     KEY = auto()
     EXIT = auto()
     TRAP = auto()
+    DOLL = auto()
 
 class MapElement:
     def __init__(self, e_type: ElementType, data: dict = None):
         self.type = e_type
-        self.data = data if data else {}
+        self.properties = data if data else {}
         self.discovered = False
 
     def interact(self, actor):
-        return self.type, self.data
+        self.discovered = True
+        return self.type, self.properties
