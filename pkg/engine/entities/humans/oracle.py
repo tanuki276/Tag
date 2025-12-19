2046efa53a2034b7a053b5ea318c150df09ee460
blowa <light06120602@gmail.com>
2025-12-19 10:46:28 +0900

    Create oracle.py

---
+++ pkg/engine/entities/humans/oracle.py
@@ -0,0 +1,14 @@
+from pkg.entities.actor import Human
+
+class Oracle(Human):
+    def __init__(self, a_id, pos, config):
+        super().__init__(a_id, pos, config)
+        self.mp = config["entities"]["human"]["base_mp"]
+        self.dream_mode = 0
+
+    def decide(self, view):
+        if self.dream_mode > 0:
+            self.dream_mode -= 1
+            # Special movement ignoring walls logic here
+        
+        return super().decide(view)
