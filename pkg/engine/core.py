aac6eac4ecc2c67c71f7b0eab74a0d215dd1058a
blowa <light06120602@gmail.com>
2025-12-19 12:04:50 +0900

    core.py の更新

--- pkg/engine/core.py
+++ pkg/engine/core.py
@@ -11,60 +11,29 @@ class SimulationCore:
         self.resolver = ActionResolver(config)
 
     def step(self):
+        active_actors = {
+            a_id: actor for a_id, actor in self.state.actor_data.items() 
+            if actor.alive and not actor.escaped
+        }
+
         views = self.mediator.get_local_views(self.state)
         
         intents = {
             a_id: actor.decide(views[a_id]) 
-            for a_id, actor in self.state.actor_data.items() 
-            if actor.alive and not actor.escaped
+            for a_id, actor in active_actors.items()
         }
         
         resolved_actions = self.resolver.resolve(intents, self.state)
         
-        self._commit_state_updates(resolved_actions)
+        self.state.apply(resolved_actions)
 
         if self.learning_cfg.get("meta_strategy", {}).get("enable_feedback_loop"):
             self.mediator.inject_learning(self.state, resolved_actions)
 
-        self.state.turn += 1
-        self._check_termination()
-
         return self.state.export_step_result(intents, resolved_actions)
 
-    def _commit_state_updates(self, resolved_actions):
-        for a_id, action in resolved_actions.items():
-            actor = self.state.actor_data[a_id]
-            
-            if action.status_update.get("alive") is False:
-                actor.alive = False
-                continue
-                
-            if action.target_pos:
-                actor.prev_pos = actor.pos
-                actor.pos = action.target_pos
-            
-            actor.update_state()
-
-    def _check_termination(self):
-        humans = [a for a in self.state.actor_data.values() if not a.is_oni]
-        
-        if self.state.exit_open:
-            for h in humans:
-                if h.alive and not h.escaped and h.pos == self.state.exit_pos:
-                    h.escaped = True
-            
-            active_humans = [h for h in humans if h.alive and not h.escaped]
-            if not active_humans and any(h.escaped for h in humans):
-                self.state.is_terminal = True
-                self.state.termination_reason = "ALL_HUMANS_ESCAPED"
-                return
-
-        active_humans = [h for h in humans if h.alive and not h.escaped]
-        if not active_humans:
-            self.state.is_terminal = True
-            self.state.termination_reason = "ALL_HUMANS_ELIMINATED"
-            return
-
-        if self.state.turn >= self.config["world"]["max_turns"]:
-            self.state.is_terminal = True
-            self.state.termination_reason = "MAX_TURNS_REACHED"
+    def run(self):
+        results = []
+        while not self.state.is_terminal:
+            results.append(self.step())
+        return results
