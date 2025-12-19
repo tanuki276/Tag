8f0d08b8683a5f64962fbedb90c460f726d56c0b
blowa <light06120602@gmail.com>
2025-12-19 11:31:33 +0900

    logger.py の更新

--- pkg/utils/logger.py
+++ pkg/utils/logger.py
@@ -4,15 +4,19 @@ import sys
 class GameLogger:
     def __init__(self, level="INFO"):
         logger.remove()
-        logger.add(sys.stderr, level=level, format="<green>{time:HH:mm:ss}</green> | <level>{message}</level>")
+        logger.add(
+            sys.stderr, 
+            level=level, 
+            format="<green>{time:HH:mm:ss}</green> | <level>{message}</level>"
+        )
         self.lib = logger
 
     def info(self, msg): self.lib.info(msg)
     def error(self, msg): self.lib.error(msg)
 
     def log_turn(self, turn, res):
-        alive = len([a for a in res.snapshot.values() if "H" in a.a_id and a.alive and not a.escaped])
-        captures = len([a for a in res.actions.values() if hasattr(a, 'status_update') and not a.status_update.get('alive', True)])
+        alive = len([a for a in res.snapshot.values() if not a["is_oni"] and a["alive"] and not a["escaped"]])
+        captures = len([a for a in res.actions.values() if a.status_update.get('alive') is False])
         msg = f"T{turn:03} | Alive: {alive} | Captures: {captures}"
         if res.is_terminal:
             msg += f" | {res.termination_reason}"
